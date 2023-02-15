0025e142fce77cb64fb9f631d14c13f70bcfb273
Diego Méndez <diegomen15@gmail.com>
2023-02-15 19:59:03 +0100

    feat: Exercise

--- loops.js
+++ loops.js
@@ -28,3 +28,54 @@ do {
   console.log(j);
   j--;
 } while(j > 0)
+
+
+// TASK 1
+let language = 'French';
+
+if(language === "Spanish"){
+  console.log('Hola mundo!');
+} else if(language === "French"){
+  console.log('Bonjour');
+} else {
+  console.log('Hello world!');
+}
+
+switch(language){
+  case 'Spanish': {
+    console.log('Hola mundo!');
+    break;
+  }
+  case 'French': {
+    console.log('Bonjour');
+    break;
+  }
+
+  default: {
+    console.log('Hello world!');
+  }
+}
+
+// Even/Odd - Exercise
+for(let i = 1; i <= 20; i++){
+  if(i % 2 === 0){
+    console.log(`${i} is even`);
+  } else {
+    console.log(`${i} is odd`);
+  }
+ 
+}
+
+// 0 to 30 - Exercise
+let k = 0;
+while(k <= 30){
+  if(k === 10){
+    console.log('ten');
+  } else if(k === 20){
+    console.log('twenty')
+  } else {
+    console.log(k);
+  }
+
+  k++;
+}
